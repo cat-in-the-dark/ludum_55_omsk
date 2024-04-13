b78960763f3227ebf5d370722c0b88808bf2d0fb
Kirill Leyfer <leyfer.kirill@gmail.com>
2024-04-13 20:29:28 +0300

    Fix balance
    Signed-off-by: Kirill Leyfer <leyfer.kirill@gmail.com>

--- src/scenes/balance.h
+++ src/scenes/balance.h
@@ -1,5 +1,5 @@
 namespace balance {
-constexpr auto kWaveSpeed = 50.0f;
+constexpr auto kWaveSpeed = 3.0f;
 constexpr auto kWaveLifetime = 5.0f;
 constexpr auto kWaveSegmentLifetime = 3.0f;
 };  // namespace balance
